d82344a082e984c18e19b3cc2d34a268a1b8de8b
Rizka Fiddiyansyah <68379809+Rizukaf-id@users.noreply.github.com>
2025-07-17 11:59:31 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from inference import chat_inference
+
+st.title("Bot Chat Sederhana dengan Gemini")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("ketik pesan disini")
+if prompt:
+    # kirim response ke chat_inference
+    response = chat_inference(prompt)
+    # tampilkan pesan dari user
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({
+        "role": "user", "content": prompt
+    })
+    # tampilkan response dari bot
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.messages.append({
+        "role": "assistant", "content": response
+    })
+
+
+
+# prompt = st.chat_input("Ketika pesan disini")
+# st.chat_message("user", prompt)
+# if prompt:
+#     response = chat_inference(prompt)
+#     with st.chat_message("assistant"):
+#         st.markdown(response)
+
